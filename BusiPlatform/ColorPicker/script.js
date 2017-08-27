c984881f7ca9641d66266512e8cd8168c177fb6b
Ezio Shiki <zzyagg@gmail.com>
2017-08-27 17:55:58 +0800

    DONE: remove color object, use chroma color object

--- BusiPlatform/ColorPicker/script.js
+++ BusiPlatform/ColorPicker/script.js
@@ -1,63 +1,3 @@
-const _Color = function(h,s,l){
-  // h in [0,360)
-  this._h = h;
-  // s in [0,1]
-  this._s = s*100;
-  // l in [0,1]
-  this._l = l*100;
-
-}
-
-_Color.prototype = {
-  constructor: _Color,
-  check:function(){
-    if(this._h<0||this._h>=360||this._s<0||this._s>100||this._l<0||this._l>100) {
-      throw new Error("Invalid Color:"+this);
-    }
-  },
-  getChromaObj:function(){
-    // TODO: seems chroma.hsl() function use much more times, find out how to reduce 
-    return chroma.hsl(this.getH(),this.getS(),this.getL());
-  },
-  hex:function(){
-    this.check();
-    let hex = this.getChromaObj().hex();
-    return hex;
-  },
-  rgb:function(){
-    this.check();
-    return this.getChromaObj().rgb();
-  },
-  hsl:function(){
-    this.check();
-    return this.getChromaObj().hsl();
-  },
-  setH:function(value){
-    this._h = value;
-    return this;
-  },
-  setS:function(value){
-    this._s = parseFloat(value.toFixed(4))*100;
-    return this;
-  },
-  setL:function(value){
-    this._l = parseFloat(value.toFixed(4))*100;
-    return this;
-  },
-  getH:function(){
-    return this._h;
-  },
-  getS:function(){
-    return this._s / 100;
-  },
-  getL:function(){
-    return this._l / 100;
-  },
-  toString:function(){
-    return "h:"+this.getH()+"; s:"+this.getS()+"; l:"+this.getL();
-  }
-}
-
 const lightPanel = {
   // how to make width and height equal to range
   range:72,
@@ -71,20 +11,21 @@ const lightPanel = {
 
 function updateColorLight(color, pos){
   const {x,y} = pos;
-  return color.setL(lightPanel.getLight({x,y}));
+  return color.set('hsl.l',lightPanel.getLight({x,y}));
 }
 
 function cloneColor(color){
-  return $.extend(true, {}, color);
+  return chroma(color.hex());
 }
 
-function drawLightPanel(color, ctx){
-  const _color = cloneColor(color);
+function renderLightPanel(color, ctx){
+  let _color = cloneColor(color);
   clearLightPanel(ctx);
   const {range, width, height} = lightPanel;
   for(let x = 0; x < width;x+=width/range){
     for(let y = 0; y < height; y+=height/range){
-      ctx.fillStyle = updateColorLight(_color, {x,y}).hex();
+      let newColor = updateColorLight(_color, {x,y});
+      ctx.fillStyle = newColor.hex();
       ctx.fillRect(x,y,Math.floor(width/range),Math.floor(height/range));
     }
   }
@@ -100,13 +41,13 @@ const hueTie = {
   height:360,
 }
 
-function drawHueTie(color, ctx){
-  const _color = cloneColor(color);
-  _color.setL(0.5);
+function renderHueTie(color, ctx){
+  let _color = cloneColor(color);
+  _color = _color.set('hsl.l',0.5);
   clearHueTie(ctx);
   const {width, height} = hueTie;
   for(let i = 0;i<height;i++){
-    _color.setH(i);
+    _color = _color.set('hsl.h',i);
     ctx.fillStyle = _color.hex();
     ctx.fillRect(0,i,width,1);
   }
@@ -118,10 +59,10 @@ function clearHueTie(ctx){
 }
 
 function updateColorHue(color, hue){
-  return color.setH(hue);
+  return color.set('hsl.h',hue);
 }
 
-function drawCircle(ctx, x, y, radius){
+function renderCircle(ctx, x, y, radius){
   
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2*Math.PI);
@@ -140,46 +81,47 @@ function getHuePos(event){
 }
 
 // TODO: when these number input changed, change color too
-function updateColorInfos(color){
+function renderColorInfos(color){
   const [r,g,b] = color.rgb();
+  const [h,s,l] = color.hsl();
   $('#r > input')[0].value = r;
   $('#g > input')[0].value = g;
   $('#b > input')[0].value = b;
-  $('#h > input')[0].value = color.getH();
-  $('#s > input')[0].value = color.getS();
-  $('#l > input')[0].value = color.getL();
+  $('#h > input')[0].value = h;
+  $('#s > input')[0].value = s;
+  $('#l > input')[0].value = l;
   
 }
 
 $(function(){
-  const color = new _Color(177,1,0.5);
+  let color = chroma.hsl(177,1,0.5);
 
   const $panel = $('#panel');
   const panelCtx = $panel[0].getContext('2d');
-  drawLightPanel(color, panelCtx);
+  renderLightPanel(color, panelCtx);
 
   const $tie = $('#tie');
   const tieCtx = $tie[0].getContext('2d');
-  drawHueTie(color, tieCtx);
+  renderHueTie(color, tieCtx);
 
   
   $panel.on('click',e=>{
     // only support Chrome! offsetX/Y is diff in Chrome and FF/IE.
     const {x, y} = getLightPos(e)
-    updateColorLight(color, {x,y});
+    color = updateColorLight(color, {x,y});
     console.log(color);
-    updateColorInfos(color);
-    drawLightPanel(color, panelCtx);
+    renderColorInfos(color);
+    renderLightPanel(color, panelCtx);
     //drawHueTie(color, tieCtx);
-    drawCircle(panelCtx, x, y, 2);
+    renderCircle(panelCtx, x, y, 2);
   })
 
   $tie.on('click', e=>{
-    updateColorHue(color, getHuePos(e));
+    color = updateColorHue(color, getHuePos(e));
     console.log(color);
-    updateColorInfos(color);
-    drawLightPanel(color, panelCtx);
-    drawHueTie(color, tieCtx);
+    renderColorInfos(color);
+    renderLightPanel(color, panelCtx);
+    renderHueTie(color, tieCtx);
     // TODO:
     // drawCircle(tieCtx, 0, y, 1);
     console.log(color);
