fc752bfa11fc8f698d1da3b59f7520be7f48a9cf
Ezio Shiki <zzyagg@gmail.com>
2017-09-03 15:44:38 +0800

    Refactor : vue-bind-4

--- src/nuomi/vue-bind-4/script.js
+++ src/nuomi/vue-bind-4/script.js
@@ -1,23 +1,7 @@
-// window.onload = function(){
-//   console.log("wind");
-// }
-// document.onload = function(){
-//   console.log("doc");
-// }
-
-// Why document.onload not working?
-
 window.onload = function() {
   const Vue = function({ el, data }) {
     let element = document.querySelectorAll(el);
-    element.forEach(function(e) {
-      e.innerHTML = e.innerHTML.replace(/{{([^}}]+)}}/g, function(
-        match,
-        group1
-      ) {
-        return eval("data." + group1);
-      });
-    });
+    parse(element, data);
   };
 
   let app = new Vue({
@@ -30,3 +14,15 @@ window.onload = function() {
     }
   });
 };
+
+/* It's a simple and unsafe implement, use eval can be injection attack, and now we only handle binding in innerHTML */
+function parse(element, data){
+  element.forEach(function(e) {
+    e.innerHTML = e.innerHTML.replace(/{{([^}}]+)}}/g, function(
+      match,
+      group1
+    ) {
+      return eval("data." + group1);
+    });
+  });
+}
