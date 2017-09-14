06f4661879ebc34a8d045e6c9db4f625585c9426
Ezio Shiki <zzyagg@gmail.com>
2017-09-14 16:35:06 +0800

    make extends class

--- src/nuomi/regex-markdown-editor/script.js
+++ src/nuomi/regex-markdown-editor/script.js
@@ -1,103 +1,68 @@
+//如果错过了这个事件怎么办？
 document.addEventListener("DOMContentLoaded", onDomReady)
 const reg = new _Regex();
 
-function onDomReady(){
-  
-  const editorIfram = document.querySelector('#editor')
-  editorIfram.contentDocument.designMode = "on";
-  const previewer = document.querySelector('#previewer')
-
-  const editorDoc = editorIfram.contentDocument;
-
-  editorDoc.addEventListener('input',function(e){
-    // log(e.target.innerText)
-    parser = new MarkdownParser(e.target.innerText);
-    parse(e.target.innerText);
-  })
-}
-
-function log(s){
-  console.log.call(console, s);
-}
-
-function parse(str){
-
-}
-
-function _Regex(){
-  const MULTI_LINE_CODE = '^(`{3}[^]*?^`{3})'
-  const NORMAL_MULTI_LINE = '^(.|\\n)+$'
-  
-  const HEADING = '^#.+$'
-  const O_LIST = '^\\d\\. .+$'
-  const U_LIST = '^\\* .+$'
-  const NEWLINE = '\\n'
-  const NORMAL_LINE_TEXT = '^.+$'
-  
-  const INLINE_CODE = '^`.+`$'
-  const NORMAL_INLINE_TEXT = '^.+$'
-
-  // NORMAL_XXXX 应该总是在数组最后，最低的匹配优先级，否则则需在其正则中排除其他正则
-  const multiLineTokens = [MULTI_LINE_CODE];
-  const lineTokens = [HEADING, O_LIST, U_LIST ,NEWLINE, NORMAL_LINE_TEXT];
-  const inlineTokens = [INLINE_CODE, NORMAL_INLINE_TEXT];
-
-  
-  return {
-    mline : new RegExp(multiLineTokens.join('|'), 'gm'),
-    line : new RegExp(lineTokens.join('|'),'gm'),
-    inline : new RegExp(inlineTokens.join('|'),'g'),
-    mlCode : new RegExp(MULTI_LINE_CODE, 'gm'),
-    lineTokens,
-    HEADING,
-    O_LIST,
-    U_LIST,
-    NEWLINE,
-    NORMAL_LINE_TEXT,
-  }
-}
-
-function MarkdownParser(str){
-  this.root = new RootNode(str)
-  log(this)
-}
-
-function RootNode(str){
+// abstract node class
+/* 
+str是此Node的源字符串
+content是此Node需要渲染的内容（去除语法标识）
+*/
+function _Node(str){
   this.str = str;
+  this.content = "";
+}
+
+function BlockLevelNode(str){
+  _Node.call(this, str);
+}
+inherit(BlockLevelNode, _Node);
+
+function LineLevelNode(str){
+  _Node.call(this, str);
+}
+inherit(LineLevelNode, _Node)
+
+function InlineLevelNode(str){
+  _Node.call(this, str);
+}
+inherit(InlineLevelNode, _Node)
+// abstract node end
+
+// Block level node
+function RootNode(str){
+  BlockLevelNode.call(this, str)
   this.multilineParse(this, str, reg);
 }
+inherit(RootNode, BlockLevelNode)
 
 RootNode.prototype.multilineParse = function(parent, str){
   const blocks = str.split(/^(`{3}[^]*?^`{3})/m)
+  const codeBlockReg = new RegExp(reg.MULTI_LINE_CODE);
   for(let block of blocks){
-    if(reg.mlCode.test(block)){
-      addNodeToChildren(parent, new MultiLineCodeNode(block))
+    if(codeBlockReg.test(block)){
+      addNodeToChildren(parent, new CodeBlockNode(block))
     }else{
-      addNodeToChildren(parent, new MultiLineNode(block))
+      addNodeToChildren(parent, new NormalBlockNode(block))
     }
   }
 
   log(parent.children)
 }
 
-function addNodeToChildren(parent, node){
-  parent.children ? 
-  parent.children.push(node) :
-  parent.children = Array.of(node);
+// In code block we do nothing now
+function CodeBlockNode(str){
+  BlockLevelNode.call(this, str);
 }
-
-// In code block we do nothing 
-function MultiLineCodeNode(str){
-  this.str = str;
-}
+inherit(CodeBlockNode, BlockLevelNode)
 
 // In normal multiline block we parse it by line
-function MultiLineNode(str){
-  this.str = str;
+function NormalBlockNode(str){
+  BlockLevelNode.call(this, str)
   this.parse(str);
 }
+inherit(NormalBlockNode, BlockLevelNode)
 
-MultiLineNode.prototype.parse = function(str){
+NormalBlockNode.prototype.parse = function(str){
   let match = null;
   const lineReg = reg.line;
   while((match = lineReg.exec(str))!==null){
@@ -133,32 +98,119 @@ MultiLineNode.prototype.parse = function(str){
     addNodeToChildren(this, node);
   }
 }
+// Block level node end
 
+// Line level node
 function NormalTextLineNode(str){
-  this.str = str;
+  LineLevelNode.call(this, str)
 }
+inherit(NormalTextLineNode, LineLevelNode)
 
 function NewLineNode(str){
-  this.str = str;
+  LineLevelNode.call(this, str)
 }
+inherit(NewLineNode, LineLevelNode)
 
 function HeadingNode(str){
-  this.str = str;
+  LineLevelNode.call(this, str)
 }
+inherit(HeadingNode, LineLevelNode)
 
 function ListItemNode(str){
-  this.str = str;
+  LineLevelNode.call(this, str)
 }
+inherit(ListItemNode, LineLevelNode)
 
 function QuoteNode(str){
-  this.str = str;
+  LineLevelNode.call(this, str)
 }
+inherit(QuoteNode, LineLevelNode)
+// Line level node end
 
+// inline level node
 function InlineCodeNode(str){
-  this.str = str;
+  InlineLevelNode.call(this, str)
+}
+inherit(InlineCodeNode, InlineLevelNode)
+// inline level node end
+
+// Utils function and class
+function MarkdownParser(str){
+  this.root = new RootNode(str)
+  log(this)
 }
 
-function CodeBlockNode(str){
-  this.str = str;
+function log(s){
+  console.log.call(console, s);
 }
+
+// 用于继承，子类需要在构造函数首行调用
+function inherit(Child, Parent){
+  let F = function(){};
+  F.prototype = Parent.prototype;
+  Child.prototype = new F();
+  Child.prototype.constructor = Child;
+}
+
+function _Regex(){
+  const MULTI_LINE_CODE = '^(`{3}[^]*?^`{3})'
+  const NORMAL_MULTI_LINE = '^(.|\\n)+$'
+  
+  const HEADING = '^#.+$'
+  const O_LIST = '^\\d\\. .+$'
+  const U_LIST = '^\\* .+$'
+  const NEWLINE = '\\n'
+  const NORMAL_LINE_TEXT = '^.+$'
+  
+  const INLINE_CODE = '^`.+`$'
+  const NORMAL_INLINE_TEXT = '^.+$'
+
+  // NORMAL_XXXX 应该总是在数组最后，最低的匹配优先级，否则则需在其正则中排除其他正则
+  const multiLineTokens = [MULTI_LINE_CODE];
+  const lineTokens = [HEADING, O_LIST, U_LIST ,NEWLINE, NORMAL_LINE_TEXT];
+  const inlineTokens = [INLINE_CODE, NORMAL_INLINE_TEXT];
+
+  
+  return {
+    mline : new RegExp(multiLineTokens.join('|'), 'gm'),
+    line : new RegExp(lineTokens.join('|'),'gm'),
+    inline : new RegExp(inlineTokens.join('|'),'g'),
+    mlCode : new RegExp(MULTI_LINE_CODE, 'gm'),
+    lineTokens,
+    HEADING,
+    O_LIST,
+    U_LIST,
+    NEWLINE,
+    NORMAL_LINE_TEXT,
+    MULTI_LINE_CODE,
+  }
+}
+
+function addNodeToChildren(parent, node){
+  parent.children ? 
+  parent.children.push(node) :
+  parent.children = Array.of(node);
+}
+// Utils end
+
+// Do it
+function onDomReady(){
+  const editorIfram = document.querySelector('#editor')
+  editorIfram.contentDocument.designMode = "on";
+  const previewer = document.querySelector('#previewer')
+
+  const editorDoc = editorIfram.contentDocument;
+
+  editorDoc.addEventListener('input',function(e){
+    // log(e.target.innerText)
+    const parser = new MarkdownParser(e.target.innerText);
+  })
+}
+
+
+
+
+
+
+
 
